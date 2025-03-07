dcd97a923852253a9b82e27b5ea4d0e2daf9eaa7
harshnandan11 <120978280+harshnandan11@users.noreply.github.com>
2025-03-07 10:42:07 +0000

    save 02_dataTypes.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "harshnandan11@gmail.com"
+var accountPassword = "12345"
+accountCity = "Indore"
+let accountState;
+
+// accountId = 2
+accountEmail = "hm@hm.in"
+accountPassword = "212121"
+accountCity = "Khargone"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //use all code as newer javascript
+
+// alert(3 + 3) //Won't work! As we're using Node.js, not browser!
+
+console.log(3 
+    +
+    3
+); //works fine, BUT Not a good practice!
+
+console.log("Harsh");
+
+let name = "Harsh";
+let age = 21;
+let isLoggedIn = false;
+let state;
+
+//number -> 2 to 2^53
+//bigint
+//string -> ""
+//boolean -> true/false
+//null -> standalone value
+//undefined
+//symbol -> unique
+
+//object
+
+console.log(typeof null);
